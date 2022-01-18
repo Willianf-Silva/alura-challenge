2e36f89e5534768aaf12e2529e899b55b5b71710
WillianF-Silva <willian.ferreira.da.silva@gmail.com>
2022-01-18 20:41:01 -0300

    Atualizando documentação swagger

--- src/main/java/br/com/wnfa/alurachallenge/dto/response/ExpenseResponseDTO.java
+++ src/main/java/br/com/wnfa/alurachallenge/dto/response/ExpenseResponseDTO.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import br.com.wnfa.alurachallenge.enums.Category;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,14 +17,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ExpenseResponseDTO {
 
+	@ApiModelProperty(notes = "Identificador da despesa", required = true, example = "01")
 	private Long id;
 	
+	@ApiModelProperty(notes = "Data da despesa", required = true, example = "30/12/2030")
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate date;
 	
+	@ApiModelProperty(notes = "Descrição da despesa", required = true, example = "Aluguel")
     private String description;
     
+    @ApiModelProperty(notes = "Categoria da despesa", required = true, example = "MORADIA")
     private Category category;
 
+    @ApiModelProperty(notes = "Valor da despesa", required = true, example = "500.00")
     private double value;
 }
