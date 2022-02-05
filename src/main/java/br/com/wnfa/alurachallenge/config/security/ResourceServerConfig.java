852ee6f628da2a507ebe37e8ac2035574ab45aca
WillianF-Silva <willian.ferreira.da.silva@gmail.com>
2022-02-05 09:36:12 -0300

    Incluindo configuracao do CORS liberando acesso a qualquer cliente

--- src/main/java/br/com/wnfa/alurachallenge/config/security/ResourceServerConfig.java
+++ src/main/java/br/com/wnfa/alurachallenge/config/security/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package br.com.wnfa.alurachallenge.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,8 +37,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http.cors().and()
+			.authorizeRequests()
 			.antMatchers(AUTH_WHITELIST).permitAll()
+			.antMatchers(HttpMethod.POST,"/api/v1/users/**").permitAll()
 			.antMatchers("/api/v1/despesas").permitAll()
 			.antMatchers("/**").authenticated()
 			.and()
