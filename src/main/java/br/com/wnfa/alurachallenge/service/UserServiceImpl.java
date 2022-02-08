eb13866080a749a7a30d1c3011d67da1bb35c93b
WillianF-Silva <willian.ferreira.da.silva@gmail.com>
2022-02-08 00:16:52 -0300

    Corrigindo bean para encodar a senha na inclusao ou alteracao do usuario.

--- src/main/java/br/com/wnfa/alurachallenge/service/UserServiceImpl.java
+++ src/main/java/br/com/wnfa/alurachallenge/service/UserServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.wnfa.alurachallenge.dto.request.UserRequestDTO;
@@ -33,7 +33,7 @@ public class UserServiceImpl implements UserService{
 	private RoleRepository roleRepository;
 	
 	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+	private PasswordEncoder passwordEncoder;
 	
 	private final UserMapper userMapper = UserMapper.INSTANCE;
 
